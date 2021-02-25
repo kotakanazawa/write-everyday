1443b0c1950b960fa89006d61f27ed86e420360c
Kota Kanazawa <kota0818@gmail.com>
2021-02-25 09:38:20 +0900

    rails g scaffold Todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "todos#index"
+  resources :todos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210225003402_add_overdue_at_column_to_todos.rb
@@ -0,0 +1,5 @@
+class AddOverdueAtColumnToTodos < ActiveRecord::Migration[6.0]
+  def change
+    add_column :todos, :overdue_at, :datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
+  end
+end
